d75ca7da00a5072f620368649cfcc5093f99d8ff
llaurrrraa <ha886275@gmail.com>
2022-01-26 09:34:51 +0800

    update index

---
+++ index.js
@@ -0,0 +1,12 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+
+createApp({
+    data(){
+        return{
+            text:'Hello World!',
+        }
+    },
+    methods:{
+        
+    }
+}).mount('#app');
